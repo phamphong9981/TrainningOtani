1c479ea7583d0f0bdbfcb31070a148e3021c7909
phamphong9981 <phamphong9981@gmail.com>
2021-06-28 13:50:29 +0700

    28062021

---
+++ WeatherTableVueJS/web/src/components/BaseCheckbox.vue
@@ -0,0 +1,19 @@
+<template>
+    <input
+      type="checkbox"
+      v-bind:checked="checked"
+      v-on:change="$emit('change', $event.target.checked)"
+    >
+</template>
+<script>
+    export default{
+        name:"base-checkbox",
+        model: {
+            prop: 'checked',
+            event: 'change'
+        },
+        props: {
+            checked: Boolean
+        },
+    }
+</script>
